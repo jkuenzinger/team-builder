9d73deac53647de9e6d8b087b4b5f56f5c849b01
jkuenzinger <jkuenzinger228@gmail.com>
2020-01-29 14:19:27 -0800

    app page set up useState declaration added form and card js files about to work on a add new member function on the card and form files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import MemberCard from './components/Card';
+import Form from './components/Form';
 import './App.css';
 
 function App() {
-  return (
+   const [members, setMembers] = useState([])
+      
+   const addNewMember = e => {
+      const newMember = {
+        id: Date.now(),
+        name: e.name,
+        email: e.email,
+        role: e.role
+      };
+      setMember([...members, newMember]); 
+   };
+   
+ return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>
+            <Form addNewMember={addNewMember} />
+            <MemberCard member={member} />
+            </code> and save to reload.
         </p>
         <a
           className="App-link"
